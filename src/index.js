d5cab1b746dcca519ba643bb54ca99170059d47c
George Hu <wuorsut@gmail.com>
2022-11-08 21:55:05 +0800

    Done sd-webui test.

--- src/index.js
+++ src/index.js
@@ -1,202 +1,161 @@
 import * as THREE from 'three';
 
-import Stats from 'three/examples/jsm/libs/stats.module.js';
-import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { OutlineEffect } from 'three/examples/jsm/effects/OutlineEffect.js';
-import { MMDLoader } from 'three/examples/jsm/loaders/MMDLoader.js';
-import { MMDAnimationHelper } from 'three/examples/jsm/animation/MMDAnimationHelper.js';
 
-let stats;
+import { OutlineEffect } from 'three/addons/effects/OutlineEffect.js';
+import { MMDLoader } from 'three/addons/loaders/MMDLoader.js';
+import { MMDAnimationHelper } from 'three/addons/animation/MMDAnimationHelper.js';
 
-let mesh, camera, scene, renderer, effect, stage;
-let helper, ikHelper, physicsHelper;
+let camera, scene, renderer, effect;
+let mesh, helper;
 
-let ready = false;
-let timeoutID;
-let prevTime = 0.0;
+const vpds = [];
 
-const api = {
-    'auto camera': true,
-    'physics on pause': true,
-    'ground shadow': true,
-    'self shadow': true,
-    'fog color': 0x43a0ad,
-    'show outline': true,
-    'show IK bones': false,
-    'show rigid bodies': false,
-    // light
-    'Hemisphere sky': 0x666666,
-    'Hemisphere ground': 0x482e2e,
-    'Directional': 0xffffff,
-};
-const gui = new GUI();
+let container;
 
-const clock = new THREE.Clock();
+document.addEventListener("DOMContentLoaded", () => {
+    waitForElm('#threejs_canvas').then((elm) => {
+        init();
+        respondToVisibility(gradioApp().getElementById("tab_3d_poser"), ()=>{
+            onWindowResize();
+        })
+    });
+})
 
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (gradioApp().querySelector(selector)) {
+            return resolve(gradioApp().querySelector(selector));
+        }
 
-Ammo().then( function () {
+        const observer = new MutationObserver(mutations => {
+            if (gradioApp().querySelector(selector)) {
+                resolve(gradioApp().querySelector(selector));
+                observer.disconnect();
+            }
+        });
 
-    init();
-    animate();
+        observer.observe(gradioApp(), {
+            childList: true,
+            subtree: true
+        });
+    });
+}
 
-} );
+function respondToVisibility(element, callback) {
+    var options = {
+      root: gradioApp().documentElement,
+    };
+  
+    var observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        callback(entry.intersectionRatio > 0);
+      });
+    }, options);
+  
+    observer.observe(element);
+  }
 
 function init() {
 
+    container = gradioApp().getElementById( 'threejs_canvas' );
 
-    const container = document.createElement( 'div' );
-    document.body.appendChild( container );
-
-    let player = document.getElementById("player")
-    // control bar
-    document.addEventListener( 'mousemove', function ( event ) {
-
-        player.style.opacity = 0.5;
-        if ( timeoutID !== undefined ) {
-            clearTimeout( timeoutID );
-        }
-
-        timeoutID = setTimeout( function () {
-            player.style.opacity = 0;
-        }, 1000 );
-    } );
+    camera = new THREE.PerspectiveCamera( 45, container.clientWidth / container.clientHeight, 1, 2000 );
+    camera.position.z = 25;
 
     // scene
-    scene = new THREE.Scene();
-    scene.fog = new THREE.Fog( api['fog color'], 10, 500 );
-    
-    // camera
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-    camera.position.set( 0, 20, 30 );
-    scene.add( camera );
-    
-    const listener = new THREE.AudioListener();
-    scene.add( listener );
-    
-    // light
-    const hemiLight = new THREE.HemisphereLight( api["Hemisphere sky"], api["Hemisphere ground"] );
-    hemiLight.position.set( 0, 40, 0 );
-    scene.add( hemiLight );
 
-    const dirLight = new THREE.DirectionalLight( api["Directional"], 0.45 );
-    dirLight.position.set( 3, 10, 10 );
-    dirLight.castShadow = true;
-    dirLight.shadow.camera.top = 25;
-    dirLight.shadow.camera.bottom = -20;
-    dirLight.shadow.camera.left = -20;
-    dirLight.shadow.camera.right = 20;
-    dirLight.shadow.camera.near = 0.1;
-    dirLight.shadow.camera.far = 80;
-    dirLight.shadow.mapSize.width = 1024;
-    dirLight.shadow.mapSize.height = 1024;
-    dirLight.shadow.bias = -0.015;
-    scene.add( dirLight );
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xffffff );
+
+    const ambient = new THREE.AmbientLight( 0x666666 );
+    scene.add( ambient );
+
+    const directionalLight = new THREE.DirectionalLight( 0x887766 );
+    directionalLight.position.set( - 1, 1, 1 ).normalize();
+    scene.add( directionalLight );
 
     // render
+
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.shadowMap.type = THREE.VSMShadowMap;
-    renderer.shadowMap.enabled = true;
+    renderer.setSize( container.clientWidth, container.clientHeight );
     container.appendChild( renderer.domElement );
+
+    // orbit controls
     const controls = new OrbitControls( camera, renderer.domElement );
-    controls.target.set( 0, 10, 0 );
 
-    // outline
     effect = new OutlineEffect( renderer );
-    effect.enabled = api['show outline']
 
-    // FPS stats
-    stats = new Stats();
-    container.appendChild( stats.dom );
-
-    // log assets downloading progress
-    let loading = document.getElementById("loading");
+    // model
 
     function onProgress( xhr ) {
 
         if ( xhr.lengthComputable ) {
 
-            const percentComplete = Math.round( xhr.loaded / xhr.total * 100, 2 ) ;
-            console.log( percentComplete + '% downloaded' );
-            loading.textContent = "Loading " + percentComplete + "%..."
+            const percentComplete = xhr.loaded / xhr.total * 100;
+            console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
 
         }
 
     }
 
-    // handle gui color change
-    function handleColorChange( color, converSRGBToLinear = false ) {
-
-        return function ( value ) {
-
-            if ( typeof value === 'string' ) {
-
-                value = value.replace( '#', '0x' );
-
-            }
-
-            color.setHex( value );
-
-            if ( converSRGBToLinear === true ) color.convertSRGBToLinear();
-
-        };
-
-    }
-
     const modelFile = 'models/mmd/つみ式ミクさんv4/つみ式ミクさんv4.pmx';
-    const vmdFiles = [ 'models/mmd/motions/GimmeGimme_with_emotion.vmd'];
-    const cameraFiles = [ 'models/mmd/cameras/GimmexGimme.vmd' ];
+    const vpdFiles = [
+        'models/mmd/motions/vpds/01.vpd',
+        'models/mmd/motions/vpds/02.vpd',
+        'models/mmd/motions/vpds/03.vpd',
+        'models/mmd/motions/vpds/04.vpd',
+        'models/mmd/motions/vpds/05.vpd',
+        'models/mmd/motions/vpds/06.vpd',
+        'models/mmd/motions/vpds/07.vpd',
+        'models/mmd/motions/vpds/08.vpd',
+        //'models/mmd/motions/vpds/09.vpd',
+        //'models/mmd/motions/vpds/10.vpd',
+        'models/mmd/motions/vpds/11.vpd'
+    ];
 
     helper = new MMDAnimationHelper();
-    
+
     const loader = new MMDLoader();
 
-    // load stage
-    loader.load('models/mmd/stages/RedialC_EpRoomDS/EPDS.pmx', function ( mesh ) {
-        mesh.castShadow = true;
-        mesh.receiveShadow = api['ground shadow'];
+    loader.load( modelFile, function ( object ) {
+
+        mesh = object;
+        mesh.position.y = - 10;
 
         scene.add( mesh );
-        stage = mesh;
-    }, onProgress, null)
 
-    // load character
-    loader.loadWithAnimation( modelFile, vmdFiles, function ( mmd ) {
+        let vpdIndex = 0;
 
-        mesh = mmd.mesh;
-        mesh.castShadow = true;
-        mesh.receiveShadow = api["self shadow"];
+        function loadVpd() {
 
-        helper.add( mesh, {
-            animation: mmd.animation,
-            physics: true
-        } );
+            const vpdFile = vpdFiles[ vpdIndex ];
 
-        loader.loadAnimation( cameraFiles, camera, function ( cameraAnimation ) {
+            loader.loadVPD( vpdFile, false, function ( vpd ) {
 
-            helper.add( camera, {
-                animation: cameraAnimation
-            } );
+                vpds.push( vpd );
 
-            scene.add(mesh);
-            ready = true;
-            loading.style.display = "none";
+                vpdIndex ++;
 
+                if ( vpdIndex < vpdFiles.length ) {
 
-        }, onProgress, null );
-        
-        ikHelper = helper.objects.get( mesh ).ikSolver.createHelper();
-        ikHelper.visible = false;
-        scene.add( ikHelper );
+                    loadVpd();
 
-        physicsHelper = helper.objects.get( mesh ).physics.createHelper();
-        physicsHelper.visible = false;
-        scene.add( physicsHelper );
+                } else {
 
-        initGui();
+                    initGui();
+                    animate();
+
+                }
+
+            }, onProgress, null );
+
+        }
+
+        loadVpd();
 
     }, onProgress, null );
 
@@ -205,65 +164,124 @@ function init() {
     window.addEventListener( 'resize', onWindowResize );
 
     function initGui() {
-        gui.add( api, 'auto camera' ).onChange( function (state) {
-            helper.enable( 'cameraAnimation', state );
-        } );
-        gui.add( api, 'physics on pause' )
 
-        guiColor(gui);
-        guiLight(gui);
-        guiShadow(gui);
-        guiDebug(gui);
-    }
-    function guiColor( gui) {
-        const folder = gui.addFolder( 'Color' );
-        folder.addColor( api, 'fog color' ).onChange( function ( value ) {
-            scene.fog.color.setHex( value );
-        });
-        folder.close();
-    }
+        const gui = new GUI();
 
-    function guiShadow( gui) {
-        const folder = gui.addFolder( 'Shadow' );
-        folder.add( api, 'ground shadow' ).onChange( function (state) {
-            stage.receiveShadow = state;
-        } );
-        folder.add( api, 'self shadow' ).onChange( function (state) {
-            mesh.receiveShadow = state;
-        } );
-        folder.close();
-    }
-    function guiLight( gui) {
-        const folder = gui.addFolder( 'Light' );
+        const dictionary = mesh.morphTargetDictionary;
 
-        folder.addColor( api, 'Directional' ).onChange( handleColorChange( dirLight.color) );
-        folder.addColor( api, 'Hemisphere sky' ).onChange( handleColorChange( hemiLight.color) );
-        folder.addColor( api, 'Hemisphere ground' ).onChange( handleColorChange( hemiLight.groundColor) );
-        folder.close();
-    }
-    function guiDebug(gui) {
-        const folder = gui.addFolder( 'Debug' );
+        const controls = {};
+        const keys = [];
 
-        folder.add( api, 'show outline' ).onChange( function (state) {
-            effect.enabled = state;
-        } );
-        folder.add( api, 'show IK bones' ).onChange( function (state) {
-            ikHelper.visible = state;
-        } );
-        folder.add( api, 'show rigid bodies' ).onChange( function (state) {
-            if ( physicsHelper !== undefined ) physicsHelper.visible = state;
-        } );
-        folder.close();
+        const poses = gui.addFolder( 'Poses' );
+        const morphs = gui.addFolder( 'Morphs' );
+
+        function getBaseName( s ) {
+
+            return s.slice( s.lastIndexOf( '/' ) + 1 );
+
+        }
+
+        function initControls() {
+
+            for ( const key in dictionary ) {
+
+                controls[ key ] = 0.0;
+
+            }
+
+            controls.pose = - 1;
+
+            for ( let i = 0; i < vpdFiles.length; i ++ ) {
+
+                controls[ getBaseName( vpdFiles[ i ] ) ] = false;
+
+            }
+
+        }
+
+        function initKeys() {
+
+            for ( const key in dictionary ) {
+
+                keys.push( key );
+
+            }
+
+        }
+
+        function initPoses() {
+
+            const files = { default: - 1 };
+
+            for ( let i = 0; i < vpdFiles.length; i ++ ) {
+
+                files[ getBaseName( vpdFiles[ i ] ) ] = i;
+
+            }
+
+            poses.add( controls, 'pose', files ).onChange( onChangePose );
+
+        }
+
+        function initMorphs() {
+
+            for ( const key in dictionary ) {
+
+                morphs.add( controls, key, 0.0, 1.0, 0.01 ).onChange( onChangeMorph );
+
+            }
+
+        }
+
+        function onChangeMorph() {
+
+            for ( let i = 0; i < keys.length; i ++ ) {
+
+                const key = keys[ i ];
+                const value = controls[ key ];
+                mesh.morphTargetInfluences[ i ] = value;
+
+            }
+
+        }
+
+        function onChangePose() {
+
+            const index = parseInt( controls.pose );
+
+            if ( index === - 1 ) {
+
+                mesh.pose();
+
+            } else {
+
+                helper.pose( mesh, vpds[ index ] );
+
+            }
+
+        }
+
+        initControls();
+        initKeys();
+        initPoses();
+        initMorphs();
+
+        onChangeMorph();
+        onChangePose();
+
+        poses.open();
+        morphs.open();
+
     }
 
 }
 
 function onWindowResize() {
 
-    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = container.clientWidth / container.clientHeight;
     camera.updateProjectionMatrix();
 
-    effect.setSize( window.innerWidth, window.innerHeight );
+    effect.setSize( container.clientWidth, container.clientHeight );
 
 }
 
@@ -272,37 +290,12 @@ function onWindowResize() {
 function animate() {
 
     requestAnimationFrame( animate );
-
-    stats.begin();
     render();
-    stats.end();
+
 }
 
 function render() {
-    if(ready){
-        // only loop once
-        if ( player.duration > 0 && player.currentTime === player.duration ){
-            gui.open();
-        }
-        let currTime = player.currentTime
-        let delta = currTime - prevTime;
 
-        if(Math.abs(delta) > 0) {
-
-            helper.update( delta , currTime);
-            prevTime = currTime
-
-        } else if(api['physics on pause']) {
-
-            let delta = clock.getDelta()
-            helper.objects.get(mesh).physics.update( delta );
-
-        }
-        if(helper.objects.get(mesh).looped) {
-            player.pause();
-            player.currentTime = 0.0;
-        }
-    }
     effect.render( scene, camera );
 
 }
