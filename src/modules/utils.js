ed1eef4e65122bb213f135e254d57d99bdeb943c
George Hu <wuorsut@gmail.com>
2022-12-03 19:29:51 +0800

    fix loading progress indictor

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,13 +1,19 @@
 // log assets downloading progress
 let loading = document.getElementById("loading");
 
+let progressMap = {};
+
 function onProgress( xhr ) {
 
     if ( xhr.lengthComputable ) {
+        let percentComplete = Math.round( xhr.loaded / xhr.total * 33, 2 ) ;
+        progressMap[xhr.total] = percentComplete;
 
-        const percentComplete = Math.round( xhr.loaded / xhr.total * 100, 2 ) ;
-        console.log( percentComplete + '% downloaded' );
-        loading.textContent = "Loading " + percentComplete + "%..."
+        let percentCompleteAll = 0;
+        for (const progress of Object.values(progressMap)) {
+            percentCompleteAll += progress;
+        }
+        loading.textContent = "Loading " + percentCompleteAll + "%...";
 
     }
 
