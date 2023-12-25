037aec3a9229b7f72b4f09debf9ab4c78adc1c48
George Hu <wuorsut@gmail.com>
2023-12-25 17:34:48 +0800

    CI: print out playwright pageerror

--- tests/fixtures/common.js
+++ tests/fixtures/common.js
@@ -5,7 +5,10 @@ export const test = base.extend({
     page: async ({ baseURL, page }, use, testInfo) => {
         // print console log of client browser
         page.on('console', msg => {
-            console.log(`${testInfo.titlePath} Client console: "${msg.text()}"`);
+            console.log(`[${testInfo.titlePath}] ${msg.text()}`);
+        });
+        page.on('pageerror', err => {
+            console.log(`[${testInfo.titlePath}] ${err.stack}`);
         });
         await use(page)
         await page.close();
