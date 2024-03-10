084bf4c5c5df34cd11531d70d03bb22787c58ce5
George Hu <wuorsut@gmail.com>
2024-03-11 01:25:08 +0800

    CI: increase timeout

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -9,14 +9,14 @@ test('has loaded', async ({ page }) => {
 test('changing camera mode with saving config', async ({ page }) => {
   // Default to Untitled preset transition
   await page.getByLabel("camera mode").selectOption({ label: "Composition" })
-  await expect(page.locator(".root > div.title")).toHaveText("Controls")
+  await expect(page.locator(".root > div.title")).toHaveText("Controls", { timeout: 100000 })
   await expect(page).toHaveScreenshot();
   await page.reload()
   await expect(page.getByLabel("camera mode")).toHaveValue("Composition")
 
   // Untitled to Untitled(moded) preset transition
   await page.getByLabel("camera mode").selectOption({ label: "Motion File" })
-  await expect(page.locator(".root > div.title")).toHaveText("Controls")
+  await expect(page.locator(".root > div.title")).toHaveText("Controls", { timeout: 100000 })
   await expect(page).toHaveScreenshot();
   await page.reload()
   await expect(page.getByLabel("camera mode")).toHaveValue("Motion File")
