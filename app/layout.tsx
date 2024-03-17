98111e863ce45f392296f6af450c424fa6fb645c
George Hu <wuorsut@gmail.com>
2024-03-18 01:50:29 +0800

    gh-page: fix relative paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  icons: { icon: "./icon.png" },
   title: "Web MMD",
   description: "Play MMD everywhere",
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    assetPrefix: './',
     reactStrictMode: false,
     output: 'export',
     webpack: (
