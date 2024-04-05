5a975cf14c5ca5ddbed7e9d08d66f149c34fc9ba
George Hu <wuorsut@gmail.com>
2024-04-05 20:10:08 +0800

    react-three: add controls event listeners

--- app/components/web-mmd/controls/index.tsx
+++ app/components/web-mmd/controls/index.tsx
@@ -2,16 +2,36 @@ import { OrbitControls } from "@react-three/drei";
 import { useEffect, useRef } from "react";
 import { OrbitControls as OrbitControlsImpl } from 'three-stdlib'
 import useGlobalStore from '@/app/stores/useGlobalStore';
+import { useThree } from "@react-three/fiber";
 
 function Controls() {
     const controlsRef = useRef<OrbitControlsImpl>()
+    const { camera } = useThree()
+    const { cwHelper } = useGlobalStore()
     useEffect(() => {
+        const controls = controlsRef.current
+        if (!controls) return
         useGlobalStore.setState({
-            controls: controlsRef.current
+            controls
         })
-    }, [controlsRef.current])
+
+        controls.domElement.addEventListener('mousedown', () => {
+            camera.up.set(0, 1, 0);
+            camera.updateProjectionMatrix();
+        });
+
+    }, [controlsRef.current, cwHelper])
+
+    const onStart = () => {
+        cwHelper.isOrbitControl = true;
+    }
+
+    const onEnd = () => {
+        cwHelper.orbitCameraPos = camera.position;
+        cwHelper.isOrbitControl = false;
+    }
     return (
-        <OrbitControls ref={controlsRef}></OrbitControls>
+        <OrbitControls onStart={onStart} onEnd={onEnd} enableDamping={false} ref={controlsRef}></OrbitControls>
     );
 }
 
