06b2e0613fe3d7491f6e235e4efc48cc7733ccaf
George Hu <wuorsut@gmail.com>
2024-06-02 16:51:18 +0800

    dispose correct objects

--- app/components/three-world/stage/index.tsx
+++ app/components/three-world/stage/index.tsx
@@ -3,7 +3,7 @@ import usePresetStore from "@/app/stores/usePresetStore";
 import { disposeMesh, onProgress } from "@/app/utils/base";
 import { useThree } from "@react-three/fiber";
 import path from "path-browserify";
-import { Suspense, useEffect } from "react";
+import { Suspense, useCallback, useEffect } from "react";
 
 function Stage() {
     const { scene } = useThree()
@@ -15,40 +15,43 @@ function Stage() {
     )
     const api = usePresetStore()
 
+    const url = api.pmxFiles.stage[api.stage]
+    const filename = api.stage
 
+    const loadStage = useCallback(async () => {
+        const stageParams = {
+            enablePBR: api['enable PBR'],
+        };
+        if (url.startsWith("data:")) {
+            Object.assign(stageParams, {
+                modelExtension: path.extname(filename).slice(1),
+                modelTextures: api.pmxFiles.modelTextures.stage[filename],
+            })
+        }
+
+        const mesh = await loader
+            .setModelParams(stageParams)
+            .loadAsync(url, onProgress)
+        const stage = mesh;
+        stage.castShadow = true;
+        stage.receiveShadow = api['ground shadow'];
+
+        if (api.stage != filename) {
+            api.stage = filename
+        }
+        useGlobalStore.setState({ stage })
+    }, [url, filename])
+
+    const disposeStage = useCallback(() => {
+        const stage = useGlobalStore.getState().stage
+        scene.remove(stage);
+        disposeMesh(stage);
+    }, [])
+    
     useEffect(() => {
-        if (!api.pmxFiles) return
-        const loadStage = async (url = api.pmxFiles.stage[api.stage], filename = api.stage) => {
-            const stageParams = {
-                enablePBR: api['enable PBR'],
-            };
-            if (url.startsWith("data:")) {
-                Object.assign(stageParams, {
-                    modelExtension: path.extname(filename).slice(1),
-                    modelTextures: api.pmxFiles.modelTextures.stage[filename],
-                })
-            }
-
-            const mesh = await loader
-                .setModelParams(stageParams)
-                .loadAsync(url, onProgress)
-            const stage = mesh;
-            stage.castShadow = true;
-            stage.receiveShadow = api['ground shadow'];
-
-            if (api.stage != filename) {
-                api.stage = filename
-            }
-            useGlobalStore.setState({ stage })
-        }
         loadStage()
-        useGlobalStore.setState({ loadStage })
-
-        return () => {
-            scene.remove(stage);
-            disposeMesh(stage);
-        }
-    }, [api.stage, api.pmxFiles])
+        return disposeStage
+    }, [])
     return (
         <Suspense fallback={null}>
             {stage ? <primitive object={stage} dispose={null} /> : null}
