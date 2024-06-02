0f4e0ebac19a2ded29dd76f80cced37e07780a6a
George Hu <wuorsut@gmail.com>
2024-06-02 16:51:18 +0800

    dispose correct objects

--- app/components/three-world/character/index.tsx
+++ app/components/three-world/character/index.tsx
@@ -4,8 +4,8 @@ import usePresetStore from "@/app/stores/usePresetStore";
 import { disposeMesh, onProgress } from "@/app/utils/base";
 import { useThree } from "@react-three/fiber";
 import path from "path-browserify";
-import { Suspense, useEffect } from "react";
-import THREE from "three";
+import { Suspense, useCallback, useEffect, useState } from "react";
+import * as THREE from 'three';
 
 function Character() {
     const { scene } = useThree()
@@ -13,64 +13,67 @@ function Character() {
     const { helper, runtimeCharacter, character, loader, updateMorphFolder } = useGlobalStore()
     const api = usePresetStore()
 
-    useEffect(() => {
-        if (!api.pmxFiles) return
-        const loadCharacter = async (url = api.pmxFiles.character[api.character], filename = api.character) => {
-            const characterParams = {
-                enableSdef: api['enable SDEF'],
-                enablePBR: api['enable PBR'],
-                followSmooth: api["follow smooth"]
-            };
-            if (url.startsWith("data:")) {
-                Object.assign(characterParams, {
-                    modelExtension: path.extname(filename).slice(1),
-                    modelTextures: api.pmxFiles.modelTextures.character[filename]
-                });
-            }
+    const url = api.pmxFiles.character[api.character]
+    const filename = api.character
 
-            const mmd = await loader.loadWithAnimation(url, api.motionFile, onProgress, () => { }, characterParams as any);
-            const character = mmd.mesh;
-            character.castShadow = true;
-            character.receiveShadow = api["self shadow"];
-
-            helper.add(character, {
-                animation: mmd.animation
+    const loadCharacter = useCallback(async () => {
+        const characterParams = {
+            enableSdef: api['enable SDEF'],
+            enablePBR: api['enable PBR'],
+            followSmooth: api["follow smooth"]
+        };
+        if (url.startsWith("data:")) {
+            Object.assign(characterParams, {
+                modelExtension: path.extname(filename).slice(1),
+                modelTextures: api.pmxFiles.modelTextures.character[filename]
             });
-            const runtimeCharacter = helper.objects.get(character)
-
-            const ikHelper = runtimeCharacter.ikSolver.createHelper();
-            ikHelper.visible = api['show IK bones'];
-            character.add(ikHelper);
-
-            const physicsHelper = runtimeCharacter.physics.createHelper();
-            physicsHelper.visible = api['show rigid bodies'];
-            helper.enable('physics', api['physics']);
-            character.add(physicsHelper);
-
-            const skeletonHelper = new THREE.SkeletonHelper(character);
-            skeletonHelper.visible = api['show skeleton'];
-            character.add(skeletonHelper);
-
-            runtimeCharacter.physics.reset();
-
-            if (api.character != filename) {
-                api.character = filename
-                updateMorphFolder()
-            }
-            useGlobalStore.setState({ character, runtimeCharacter })
         }
 
-        loadCharacter()
-        useGlobalStore.setState({ loadCharacter })
+        const mmd = await loader.loadWithAnimation(url, api.motionFile, onProgress, () => { }, characterParams as any);
+        const character = mmd.mesh;
+        character.castShadow = true;
+        character.receiveShadow = api["self shadow"];
 
-        return () => {
+        helper.add(character, {
+            animation: mmd.animation
+        });
+        const runtimeCharacter = helper.objects.get(character)
+
+        const ikHelper = runtimeCharacter.ikSolver.createHelper();
+        ikHelper.visible = api['show IK bones'];
+        character.add(ikHelper);
+
+        const physicsHelper = runtimeCharacter.physics.createHelper();
+        physicsHelper.visible = api['show rigid bodies'];
+        helper.enable('physics', api['physics']);
+        character.add(physicsHelper);
+
+        const skeletonHelper = new THREE.SkeletonHelper(character);
+        skeletonHelper.visible = api['show skeleton'];
+        character.add(skeletonHelper);
+
+        runtimeCharacter.physics.reset();
+
+        if (api.character != filename) {
+            api.character = filename
+            updateMorphFolder()
+        }
+        useGlobalStore.setState({ character, runtimeCharacter })
+    }, [url, filename])
+
+    const disposeCharacter = useCallback(
+        () => {
+            const character = useGlobalStore.getState().character
             runtimeCharacter.mixer.uncacheRoot(character);
             scene.remove(character);
             helper.remove(character);
             disposeMesh(character);
-        }
+        }, [])
 
-    }, [api.character, api.pmxFiles])
+    useEffect(() => {
+        loadCharacter()
+        return disposeCharacter
+    }, [])
 
     return (
         <Suspense fallback={null}>
