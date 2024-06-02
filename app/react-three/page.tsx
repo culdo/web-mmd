4ce3ef5767ad7089b6a4ac423d7396421bcf716f
George Hu <wuorsut@gmail.com>
2024-06-02 14:43:15 +0800

    rename threeWorld folder, dispose when unmount

--- app/react-three/page.tsx
+++ app/react-three/page.tsx
@@ -2,15 +2,14 @@
 
 import FileSelector from "@/app/components/file-selector";
 import SceneTimeline from "@/app/components/scene-timeline";
-import ThreeWorld from "@/app/components/threeWorld";
+import ThreeWorld from "@/app/components/three-world";
 import { Canvas } from "@react-three/fiber";
-import { Bloom, DepthOfField, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
 
 import Script from "next/script";
 import { useEffect } from "react";
 import ControlBar from "../components/control-bar";
 import Panel from "../components/panel";
-import OutlinePass from "../components/effects/OutlinePass";
+import Effects from "../components/effects";
 
 export default function Page() {
   useEffect(() => {
@@ -22,9 +21,7 @@ export default function Page() {
       <SceneTimeline></SceneTimeline>
       <Canvas>
         <ThreeWorld></ThreeWorld>
-        <EffectComposer>
-          <OutlinePass></OutlinePass>
-        </EffectComposer>
+        <Effects></Effects>
       </Canvas>
       <ControlBar></ControlBar>
       <FileSelector></FileSelector>
