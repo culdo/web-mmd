1ac533222be80db7265091ea6fce4eb65277c840
George Hu <wuorsut@gmail.com>
2024-06-02 13:54:10 +0800

    add outline

--- app/react-three/page.tsx
+++ app/react-three/page.tsx
@@ -4,10 +4,13 @@ import FileSelector from "@/app/components/file-selector";
 import SceneTimeline from "@/app/components/scene-timeline";
 import ThreeWorld from "@/app/components/threeWorld";
 import { Canvas } from "@react-three/fiber";
+import { Bloom, DepthOfField, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
+
 import Script from "next/script";
 import { useEffect } from "react";
 import ControlBar from "../components/control-bar";
 import Panel from "../components/panel";
+import OutlinePass from "../components/effects/OutlinePass";
 
 export default function Page() {
   useEffect(() => {
@@ -19,6 +22,9 @@ export default function Page() {
       <SceneTimeline></SceneTimeline>
       <Canvas>
         <ThreeWorld></ThreeWorld>
+        <EffectComposer>
+          <OutlinePass></OutlinePass>
+        </EffectComposer>
       </Canvas>
       <ControlBar></ControlBar>
       <FileSelector></FileSelector>
