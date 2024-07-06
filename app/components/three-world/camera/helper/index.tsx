d9e07003751f6576c04e66207bfe45928964b96e
George Hu <wuorsut@gmail.com>
2024-07-06 14:21:03 +0800

    refactor camera mode control

--- app/components/three-world/camera/helper/index.tsx
+++ app/components/three-world/camera/helper/index.tsx
@@ -2,9 +2,50 @@ import { CameraMode } from "@/app/modules/MMDCameraWorkHelper";
 import usePresetStore from "@/app/stores/usePresetStore";
 import CompositeMode from "./composite-mode";
 import MotionFileMode from "./motion-file-mode";
+import FixFollowMode from "./fix-follow-mode";
+import { useLayoutEffect } from "react";
+import { useControls } from "leva";
 
 function CameraWorkHelper() {
     const cameraMode = usePresetStore(state => state["camera mode"])
+
+    const getCameraMode = () => usePresetStore.getState()['camera mode']
+    const setCameraMode = (val: number) => usePresetStore.setState({ "camera mode": val })
+
+    const [, set] = useControls(() => ({
+        'camera mode': {
+            value: getCameraMode(),
+            options: {
+                "Motion File": CameraMode.MOTION_FILE,
+                "Composition": CameraMode.COMPOSITION,
+                "Fixed Follow": CameraMode.FIXED_FOLLOW
+            },
+            onChange: (motionType, _, options) => {
+                if (!options.initial) {
+                    setCameraMode(motionType)
+                }
+            },
+            order: 0,
+        },
+    }))
+
+    useLayoutEffect(() => {
+        // keyboard shortcuts
+        document.addEventListener("keydown", (e) => {
+            if (e.key == "`") {
+                const isEditMode = getCameraMode() != CameraMode.MOTION_FILE
+                let targetMode;
+                if (isEditMode) {
+                    targetMode = CameraMode.MOTION_FILE
+                } else {
+                    targetMode = CameraMode.COMPOSITION
+                }
+                set({ "camera mode": targetMode })
+
+            }
+        })
+    }, [])
+
     return (
         <>
             {
@@ -12,6 +53,9 @@ function CameraWorkHelper() {
             }
             {
                 cameraMode == CameraMode.MOTION_FILE && <MotionFileMode></MotionFileMode>
+            }
+            {
+                cameraMode == CameraMode.FIXED_FOLLOW && <FixFollowMode></FixFollowMode>
             }
         </>
     );
