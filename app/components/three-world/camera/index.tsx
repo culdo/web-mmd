fb8fde26f694e4343c0bee7b31034011e048ce8f
George Hu <wuorsut@gmail.com>
2024-07-06 08:14:16 +0800

    CameraWork: add MotionFile mode

--- app/components/three-world/camera/index.tsx
+++ app/components/three-world/camera/index.tsx
@@ -1,41 +1,20 @@
-import { CameraMode } from "@/app/modules/MMDCameraWorkHelper";
-import useGlobalStore from "@/app/stores/useGlobalStore";
 import usePresetStore from "@/app/stores/usePresetStore";
-import { onProgress } from "@/app/utils/base";
 import { PerspectiveCamera } from "@react-three/drei";
-import { useLayoutEffect, useRef } from "react";
+import { useRef } from "react";
 import { PerspectiveCamera as PerspectiveCameraImpl } from "three";
 import CameraWorkHelper from "./helper";
 
 function Camera() {
     const cameraRef = useRef<PerspectiveCameraImpl>()
-
-    const helper = useGlobalStore(state => state.helper)
-    const loader = useGlobalStore(state => state.loader)
-    const cameraFile = usePresetStore(state => state.cameraFile)
-    const cameraMode = usePresetStore(state => state["camera mode"])
     const fov = usePresetStore(state => state.fov)
     const near = usePresetStore(state => state.near)
     const zoom = usePresetStore(state => state.zoom)
 
-    useLayoutEffect(() => {
-        const loadCamera = async () => {
-
-            const cameraAnimation = await loader.loadAnimation(cameraFile, cameraRef.current, onProgress);
-            helper.add(cameraRef.current, {
-                animation: cameraAnimation,
-                enabled: cameraMode == CameraMode.MOTION_FILE
-            });
-
-            useGlobalStore.setState({ loadCamera })
-        }
-        loadCamera()
-
-    }, [cameraFile])
-
     return (
         <>
-            <PerspectiveCamera ref={cameraRef} fov={fov} near={near} zoom={zoom} position={[0, 10, 50]} makeDefault />
+            <PerspectiveCamera ref={cameraRef} fov={fov} near={near} zoom={zoom} position={[0, 10, 50]} makeDefault>
+                <object3D name="target" userData={{ frameNum: 0 }} />
+            </PerspectiveCamera>
             <CameraWorkHelper></CameraWorkHelper>
         </>
     )
