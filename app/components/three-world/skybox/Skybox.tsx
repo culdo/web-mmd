106de98ccd0b94a1ea5da8001d3890c85376c388
George Hu <wuorsut@gmail.com>
2024-10-15 23:18:49 +0800

    add Skybox

---
+++ app/components/three-world/skybox/Skybox.tsx
@@ -0,0 +1,29 @@
+import { buildGuiItem } from "@/app/utils/gui";
+import { useLoader, useThree } from "@react-three/fiber";
+import { useControls } from "leva";
+import { useEffect } from "react";
+import * as THREE from "three";
+import { RGBELoader } from "three-stdlib";
+
+function Skybox({ hdrUrl }: { hdrUrl: string }) {
+    const { gl, scene } = useThree();
+    const texture = useLoader(RGBELoader, hdrUrl);
+
+    useControls("Skybox", {
+        "envIntensity": buildGuiItem("envIntensity", (value) => {
+            scene.environmentIntensity = value
+        })
+    })
+    useEffect(() => {
+        const pmremGenerator = new THREE.PMREMGenerator(gl);
+        pmremGenerator.compileEquirectangularShader();
+
+        const envMap = pmremGenerator.fromEquirectangular(texture).texture;
+        envMap.colorSpace = THREE.SRGBColorSpace;
+        envMap.mapping = THREE.EquirectangularReflectionMapping;
+        scene.environment = envMap
+    }, [hdrUrl])
+    return <></>
+}
+
+export default Skybox;
