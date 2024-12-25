d8c9d5c42dbc3f43f20bf2e760f9ed458c0c3789
George Hu <wuorsut@gmail.com>
2024-12-25 22:47:42 +0800

    fix dev server hmr

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    assetPrefix: './',
-    reactStrictMode: false,
-    output: 'export',
-    webpack: (
-        config
-    ) => {
-        config.module.rules.push({
-            test: /\.(vert|frag)$/,
-            loader: 'raw-loader'
-        })
-        config.resolve.fallback = { fs: false };
-        return config
-    },
-};
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js'
 
-export default nextConfig;
+export default (phase) => {
+    const isDev = phase === PHASE_DEVELOPMENT_SERVER
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+        assetPrefix: isDev ? undefined : './',
+        reactStrictMode: false,
+        output: 'export',
+        webpack: (
+            config
+        ) => {
+            config.module.rules.push({
+                test: /\.(vert|frag)$/,
+                loader: 'raw-loader'
+            })
+            config.resolve.fallback = { fs: false };
+            return config
+        },
+    };
+    return nextConfig
+  }
+
