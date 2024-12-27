5adea8372067fb01be46df2cb2645a694467e0e0
George Hu <wuorsut@gmail.com>
2024-12-28 01:11:57 +0800

    add leva controlled collapsed

--- app/components/panel/index.tsx
+++ app/components/panel/index.tsx
@@ -1,14 +1,40 @@
 import useGlobalStore from "@/app/stores/useGlobalStore";
 import { Leva } from "leva";
 import usePreset from "./preset";
+import styles from "./styles.module.css"
+import { useEffect, useState } from "react";
 
 function Panel() {
     const gui = useGlobalStore(state => state.gui)
     usePreset()
     const presetReady = useGlobalStore(states => states.presetReady)
     const hidden = !presetReady || gui.hidden
+    const [collapsed, setCollapsed] = useState(false)
+
+    function handleWindowSizeChange() {
+        if (window.innerWidth < 1000) {
+            setCollapsed(true)
+        }
+    }
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+    }, []);
+
     return (
-        <Leva oneLineLabels {...gui} hidden={hidden}></Leva>
+        <div className={styles.levaContainer}>
+            <Leva
+                oneLineLabels
+                {...gui}
+                collapsed={{
+                    collapsed: collapsed,
+                    onChange: (state) => setCollapsed(state)
+                }}
+                hidden={hidden}>
+            </Leva>
+        </div>
     );
 }
 
