0f8bf5a72755f9561c8eae5e09285abcc5b6d1de
George Hu <wuorsut@gmail.com>
2025-03-15 21:49:19 +0800

    add bone controller

--- app/components/three-world/character/index.tsx
+++ app/components/three-world/character/index.tsx
@@ -3,11 +3,13 @@ import Morph from "../ModelHelper/Morph";
 import Material from "../ModelHelper/Material";
 import Physics from "../ModelHelper/Physics";
 import Animation from "../ModelHelper/Animation";
+import Bone from "../ModelHelper/Bone";
 
 function Character() {
     return (
         <Model>
             <Morph></Morph>
+            <Bone></Bone>
             <Material></Material>
             <Physics></Physics>
             <Animation></Animation>
