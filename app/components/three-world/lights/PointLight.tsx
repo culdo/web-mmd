c119d1d9485c02cd113b979d5d03c0d956d15101
George Hu <wuorsut@gmail.com>
2025-08-18 00:37:11 +0800

    add point light visible option

--- app/components/three-world/lights/PointLight.tsx
+++ app/components/three-world/lights/PointLight.tsx
@@ -1,6 +1,7 @@
 import useGlobalStore from "@/app/stores/useGlobalStore"
 import usePresetStore from "@/app/stores/usePresetStore"
 import { buildFlexGuiItem } from "@/app/utils/gui"
+import { Sphere } from "@react-three/drei"
 import { button, folder, useControls } from "leva"
 
 function PointLight({ name }: { name?: string }) {
@@ -16,6 +17,9 @@ function PointLight({ name }: { name?: string }) {
                 min: 0,
                 max: 10
             },
+            visible: {
+                value: false
+            },
             position: buildFlexGuiItem<[number, number, number]>(`${guiName}.position`),
             select: button(() => useGlobalStore.setState({ selectedName: `${guiName}.position` })),
             delete: button(() => usePresetStore.setState(({ Light }) => {
@@ -26,7 +30,14 @@ function PointLight({ name }: { name?: string }) {
     }, [presetReady])
     return (
         <>
-            <pointLight name={`${guiName}.position`} color={pointLight.color} position={pointLight.position} intensity={pointLight.intensity} />
+            <pointLight name={`${guiName}.position`} color={pointLight.color} position={pointLight.position} intensity={pointLight.intensity}>
+                {
+                    pointLight.visible &&
+                    <Sphere scale={0.2}>
+                        <meshStandardMaterial emissive={pointLight.color} emissiveIntensity={1.0} color={pointLight.color} />
+                    </Sphere>
+                }
+            </pointLight>
         </>
     );
 }
