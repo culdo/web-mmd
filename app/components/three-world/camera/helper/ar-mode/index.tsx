9ed131177c7b5ad767e1bc573d588af77479532e
George Hu <wuorsut@gmail.com>
2025-12-19 08:07:33 +0800

    add ar camera scale slider

--- app/components/three-world/camera/helper/ar-mode/index.tsx
+++ app/components/three-world/camera/helper/ar-mode/index.tsx
@@ -1,12 +1,24 @@
 import useWebRTC from "@/app/ar-camera/useWebrtc";
 import { useThree } from "@react-three/fiber";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { closeSnackbar, enqueueSnackbar } from "notistack";
+import { useControls } from "leva";
+import { buildGuiItem } from "@/app/utils/gui";
 
 function ARCameraMode() {
     const dataChannel = useWebRTC();
     const camera = useThree(state => state.camera);
-
+    const scaleRef = useRef<number>()
+    useControls("AR Camera", {
+        scale: {
+            ...buildGuiItem("ar camera scale"),
+            min: 100,
+            max: 1000,
+            onChange: (value) => {
+                scaleRef.current = value
+            }
+        }
+    })
     useEffect(() => {
         enqueueSnackbar("Waiting for AR session...", { persist: true, key: "ar-webrtc-connect" });
         if (!dataChannel) return () => closeSnackbar("ar-webrtc-connect");
@@ -17,9 +29,9 @@ function ARCameraMode() {
 
         const handleMessage = (event: MessageEvent) => {
             camera.matrixWorld.fromArray(JSON.parse(event.data));
-            camera.matrixWorld.elements[12] *= 100
-            camera.matrixWorld.elements[13] *= 100
-            camera.matrixWorld.elements[14] *= 100
+            camera.matrixWorld.elements[12] *= scaleRef.current
+            camera.matrixWorld.elements[13] *= scaleRef.current
+            camera.matrixWorld.elements[14] *= scaleRef.current
         };
 
         dataChannel?.addEventListener('message', handleMessage);
