e012289c30b379c783de29eace83f9b431709ccd
George Hu <wuorsut@gmail.com>
2026-02-01 06:05:24 +0800

    temp fix WebGL PCFSoftShadowMap

--- app/components/three-world/lights/DirectionalLight.tsx
+++ app/components/three-world/lights/DirectionalLight.tsx
@@ -1,35 +1,33 @@
 import useGlobalStore from "@/app/stores/useGlobalStore"
 import usePresetStore from "@/app/stores/usePresetStore"
 import { buildFlexGuiItem } from "@/app/utils/gui"
-import { button, folder, useControls } from "leva"
+import { button, useControls } from "leva"
 
 function DirectionalLight({ name }: { name?: string }) {
     const shadowBias = usePresetStore(state => state["shadow bias"])
 
     const guiName = `Light.${name}`
 
-    const directionalLight = useControls('Light', {
-        [name]: folder({
-            color: buildFlexGuiItem<string>(`${guiName}.color`),
-            intensity: {
-                ...buildFlexGuiItem<number>(`${guiName}.intensity`),
-                min: 0,
-                max: 10
-            },
-            position: buildFlexGuiItem<[number, number, number]>(`${guiName}.position`),
-            castShadow: buildFlexGuiItem<boolean>(`${guiName}.castShadow`),
-            select: button(() => useGlobalStore.setState({ selectedName: `${guiName}.position` })),
-            delete: button(() => usePresetStore.setState(({ Light }) => {
-                delete Light[name]
-                return { Light: { ...Light } }
-            }))
-        })
+    const directionalLight = useControls(guiName, {
+        color: buildFlexGuiItem<string>(`${guiName}.color`),
+        intensity: {
+            ...buildFlexGuiItem<number>(`${guiName}.intensity`),
+            min: 0,
+            max: 10
+        },
+        position: buildFlexGuiItem<[number, number, number]>(`${guiName}.position`),
+        castShadow: buildFlexGuiItem<boolean>(`${guiName}.castShadow`),
+        select: button(() => useGlobalStore.setState({ selectedName: `${guiName}.position` })),
+        delete: button(() => usePresetStore.setState(({ Light }) => {
+            delete Light[name]
+            return { Light: { ...Light } }
+        }))
     })
     return (
         <>
-            <directionalLight castShadow={directionalLight.castShadow} name={`${guiName}.position`} 
-            color={directionalLight.color} position={directionalLight.position} intensity={directionalLight.intensity}
-            shadow-mapSize={[1024, 1024]} shadow-bias={shadowBias}>
+            <directionalLight castShadow={directionalLight.castShadow} name={`${guiName}.position`}
+                color={directionalLight.color} position={directionalLight.position} intensity={directionalLight.intensity}
+                shadow-mapSize={[1024, 1024]} shadow-bias={shadowBias}>
                 <orthographicCamera attach="shadow-camera" args={[-20, 20, 25, -20, 0.1, 80]} />
             </directionalLight>
         </>
--- app/components/three-world/renderer/useRenderer.ts
+++ app/components/three-world/renderer/useRenderer.ts
@@ -2,8 +2,19 @@ import useGlobalStore from "@/app/stores/useGlobalStore"
 import { buildGuiObj } from "@/app/utils/gui"
 import { useControls } from "leva"
 import useRenderLoop from "./useRenderLoop"
+import { useThree } from "@react-three/fiber"
+import { useEffect } from "react"
+import { PCFShadowMap, PCFSoftShadowMap, WebGLRenderer } from "three"
 
 function useRenderer() {
+    // Temporarily fix a WebGL PCFSoftShadowMap bug in three.js r182 , which is fixed in r183.
+    const renderer = useThree(state => state.gl)
+    useEffect(() => {
+        if (renderer instanceof WebGLRenderer && renderer.shadowMap.type == PCFSoftShadowMap) {
+            renderer.shadowMap.type = PCFShadowMap
+        }
+    }, [renderer.shadowMap.type])
+    
     useControls("Renderer", {
         ...buildGuiObj("set pixelratio 1.0"),
         ...buildGuiObj("enable PBR"),
