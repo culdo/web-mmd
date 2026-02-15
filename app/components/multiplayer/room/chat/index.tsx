66a74edaf81240df97fb2eeccf61880578ca49a7
George Hu <wuorsut@gmail.com>
2026-02-15 19:39:32 +0800

    make Chat UI simple

--- app/components/multiplayer/room/chat/index.tsx
+++ app/components/multiplayer/room/chat/index.tsx
@@ -35,9 +35,14 @@ function Chat() {
             })
         }
         inputRef.current.onkeydown = (e) => {
+            e.stopPropagation()
             if (e.key == "Enter") {
                 const text = inputRef.current.value
-                if (!text) return
+                if (!text) {
+                    inputRef.current.classList.add("invisible")
+                    inputRef.current.blur()
+                    return
+                }
                 if (groupChannels["model"]) {
                     groupChannels["model"].sharedObjects.chatTextRef.current.text = text
                 }
@@ -50,10 +55,20 @@ function Chat() {
                 })
                 channel.send(text)
                 inputRef.current.value = ""
-                inputRef.current.blur()
             }
         }
     }, [channel])
+
+    useEffect(() => {
+        const onKeyDown = (e: KeyboardEvent) => {
+            if (e.key == "Enter") {
+                inputRef.current.classList.remove("invisible")
+                inputRef.current.focus()
+            }
+        }
+        document.addEventListener("keydown", onKeyDown)
+        return () => document.removeEventListener("keydown", onKeyDown)
+    }, [])
 
     useEffect(() => {
         textareaRef.current.scrollTop = textareaRef.current.scrollHeight
@@ -70,15 +85,15 @@ function Chat() {
 
     return (
         <>
-            <div ref={containerRef} className={styles.chat}>
-                <div ref={textareaRef} className="px-2 h-full bg-white bg-opacity-10 rounded-t-lg hover:overflow-auto overflow-hidden flex flex-col">
+            <div tabIndex={-1} ref={containerRef} className={`${styles.chat} group`}>
+                <div ref={textareaRef} className="px-2 min-h-10 bg-white bg-opacity-0 group-focus:bg-opacity-10 rounded-t-lg group-focus:overflow-auto overflow-hidden flex flex-col">
                     {
                         Object.entries(texts).map(
                             ([i, text]) => <div key={i} className={text.style}>{text.node}</div>
                         )
                     }
                 </div>
-                <input name="input" ref={inputRef} className="px-2 rounded-b-md" placeholder="sending message..." type="text"></input>
+                <input name="input" ref={inputRef} className="px-2 rounded-b-md invisible hover:visible focus:visible group-focus:visible" placeholder="sending message..." type="text"></input>
             </div>
         </>
     );
